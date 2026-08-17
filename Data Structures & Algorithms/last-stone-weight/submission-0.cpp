9ad332e6360c79f2a2eda131e7ee1125a62793f2
Nalin Prabhath <48054155+nalin2002@users.noreply.github.com>
2026-08-16 23:17:52 -0500

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n=stones.size();
+        priority_queue<int> pq(stones.begin(),stones.end());
+
+        while(!pq.empty() && pq.size()>1){
+            int x= pq.top();
+            pq.pop();
+
+            int y= pq.top();
+            pq.pop();
+
+            if(x==y){
+                continue;
+            }
+            else{
+                pq.push(x-y);
+            }
+        }
+        return pq.empty()?0:pq.top();
+    }
+};
