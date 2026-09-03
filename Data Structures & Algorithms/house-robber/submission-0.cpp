324d1dabf9572de26e0220570dccb95c63e2768f
Nalin Prabhath <48054155+nalin2002@users.noreply.github.com>
2026-09-03 13:25:35 -0500

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dp;
+    int max_money(vector<int>& nums,int index){
+        if(index >= nums.size()){
+            return 0;
+        }
+        if(dp[index]!=-1) return dp[index];
+        
+        int m1= nums[index]+ max_money(nums,index+2);
+
+        int m2= max_money(nums,index+1);
+
+        return dp[index]=max(m1,m2);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        dp.resize(n+1,-1);
+        return max_money(nums,0);
+    }
+};
